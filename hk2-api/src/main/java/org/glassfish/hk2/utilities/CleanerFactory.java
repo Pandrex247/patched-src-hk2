d2c8a22400048f6ab3e26f4c34114a214141387c
Andrew Pielage <pandrex247@hotmail.com>
2024-08-13 11:37:44 +0100

    Use explicit type argument
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

--- hk2-api/src/main/java/org/glassfish/hk2/utilities/CleanerFactory.java
+++ hk2-api/src/main/java/org/glassfish/hk2/utilities/CleanerFactory.java
@@ -30,7 +30,7 @@ public final class CleanerFactory {
     private final static Cleaner commonCleaner = Cleaner.create(new ThreadFactory() {
         @Override
         public Thread newThread(final Runnable r) {
-            return AccessController.doPrivileged(new PrivilegedAction<>() {
+            return AccessController.doPrivileged(new PrivilegedAction<Thread>() {
                 @Override
                 public Thread run() {
                     Thread t = new Thread(null, r, "Common-Cleaner");
